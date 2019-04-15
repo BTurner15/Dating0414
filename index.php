209fc27ee42e503641379935769d8cae0ddd0654
Bruce Turner <you@example.com>
2019-04-14 21:45:13 -0400

    Adding comments

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 
 <?php
+// Bruce Turner, Professor Ostrander, Spring 2019 -->
+// IT 328 Full STack Web Development -->
+// Dating 1 Assignment -->
+// file: index.php  --> default landing page, defines default route
+//
+//
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 require_once('vendor/autoload.php');
@@ -7,7 +13,6 @@ $f3 = Base::instance();
 $f3->set('DEBUG',3);
 //Define a default route
 $f3->route('GET /', function(){
-    //echo '<h1>via GET Thurs. 04-11</h1>';
     //echo '<h1>Hola!</h1>';
     //display a view
     $view = new View();
